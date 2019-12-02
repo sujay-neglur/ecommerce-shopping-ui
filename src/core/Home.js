8c4fc9ba772d218f82401fbf5836ce9671570f92
Sujay Neglur <sujayneglur123@gmail.com>
2019-12-01 19:50:09 -0500

    added div columns here instead of Card component

--- src/core/Home.js
+++ src/core/Home.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
 import { getProducts } from "./apiCore";
 import Card from "./Card";
+import Search from "./Search";
 
 const Home = () => {
   const [productsBySell, setProductsBySell] = useState([]);
@@ -36,16 +37,21 @@ const Home = () => {
       description="Node React E-commerce App"
       className="container-fluid"
     >
+      <Search />
       <h2 className="mb-4">New Arrivals</h2>
       <div className="row">
         {productsByArrival.map((product, index) => (
-          <Card key={index} product={product} />
+          <div className="col-4 mb-3" key={index}>
+            <Card product={product} />
+          </div>
         ))}
       </div>
       <h2 className="mb-4">Best Sellers</h2>
       <div className="row">
         {productsBySell.map((product, index) => (
-          <Card key={index} product={product} />
+          <div className="col-4 mb-3" key={index}>
+            <Card product={product} />
+          </div>
         ))}
       </div>
     </Layout>
