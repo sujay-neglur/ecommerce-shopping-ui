9d037fb56c0243a7d5c0924d4b4bc83155fab6e4
Sujay Neglur <sujayneglur123@gmail.com>
2019-12-19 18:50:27 -0500

    Before sys upgrade

--- src/Routes.js
+++ src/Routes.js
@@ -11,6 +11,7 @@ import AddCategory from "./admin/AddCategory";
 import AddProduct from "./admin/AddProduct";
 import Shop from "./core/Shop";
 import Product from "./core/Product";
+import Cart from "./core/Cart";
 
 const Routes = () => {
   return (
@@ -25,6 +26,7 @@ const Routes = () => {
         <AdminRoute component={AddCategory} path="/create/category" exact />
         <AdminRoute component={AddProduct} path="/create/product" exact />
         <Route exact path="/product/:productId" component={Product} />
+        <PrivateRoute exact path="/cart" component={Cart}/>
       </Switch>
     </BrowserRouter>
   );
---
+++ src/core/Cart.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import Layout from "./Layout";
+
+const Cart = () => {
+  return (
+    <Layout
+    title="Add To Cart Page"
+    description="Node React E-commerce App"
+    className="container-fluid"
+  >
+    <h1>Cart Page</h1>
+  </Layout>
+  )
+};
+
+export default Cart;
