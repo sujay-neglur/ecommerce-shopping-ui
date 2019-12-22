b7e2c8c5e0209c6a1ce4efc9048a4645826804dc
Sujay Neglur <sujayneglur123@gmail.com>
2019-12-22 00:27:05 -0500

    created cart page

--- src/core/Cart.js
+++ src/core/Cart.js
@@ -1,16 +1,63 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
+import { getCart, removeItem } from "./cartHelpers";
+import { Link } from "react-router-dom";
+import Card from "./Card";
+import Checkout from "./Checkout";
 
 const Cart = () => {
+  const [items, setItems] = useState([]);
+  const [run, setRun] = useState([]);
+
+
+  useEffect(() => {
+    setItems(getCart());
+  }, [run]);
+
+  const showItems = items => {
+    return (
+      <div>
+        <h2>Your cart has {items.length} items</h2>
+        <hr />
+        {items.map((product, index) => (
+          <Card
+            key={index}
+            product={product}
+            showAddToCartButton={false}
+            cartUpdate={true}
+            showRemoveProductButton={true}
+            run={run}
+            setRun={setRun}
+          />
+        ))}
+      </div>
+    );
+  };
+
+  const noItemsMessage = () => (
+    <h2>
+      Your cart is empty. <br /> <Link to="/shop">Continue Shopping</Link>
+    </h2>
+  );
+
   return (
     <Layout
-    title="Add To Cart Page"
-    description="Node React E-commerce App"
-    className="container-fluid"
-  >
-    <h1>Cart Page</h1>
-  </Layout>
-  )
+      title="Shopping Cart"
+      description="Manage your cart items. Add, Remove, Checkout or Continue Shopping"
+      className="container-fluid"
+    >
+      <div className="row">
+        <div className="col-6">
+          {items.length > 0 ? showItems(items) : noItemsMessage()}
+        </div>
+        <div className="col-6">
+          <h2 className="mb-4">Your Cart Summary</h2>
+          <hr/>
+          <Checkout products={items}/>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Cart;
